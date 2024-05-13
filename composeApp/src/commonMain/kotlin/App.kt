43b256381ea8230543b6a305ecf9f9778a45f3e4
mejdi14 <mejdiclubiste@gmail.com>
2024-05-13 10:23:35 +0200

    make calculation

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -5,14 +5,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -23,12 +24,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.imageResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -50,44 +51,36 @@ fun App() {
             Res.drawable.image_1, Res.drawable.image_2,
             Res.drawable.image_1, Res.drawable.image_2,
             Res.drawable.image_1, Res.drawable.image_2,
-
-            )
+            Res.drawable.image_1, Res.drawable.image_2,
+            Res.drawable.image_1, Res.drawable.image_2,
+            Res.drawable.image_1, Res.drawable.image_2,
+            Res.drawable.image_1, Res.drawable.image_2,
+            Res.drawable.image_1, Res.drawable.image_2,
+            Res.drawable.image_1,
+            Res.drawable.image_2,
+            Res.drawable.image_1,
+            Res.drawable.image_2,
+        )
         val listState = rememberLazyListState()
-        val centerOfViewport = remember {
-            { (listState.layoutInfo.viewportStartOffset + listState.layoutInfo.viewportEndOffset) / 2 }
-        }
-
         LazyRow(
-            state = listState,
             modifier = Modifier.fillMaxSize(),
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            state = listState
         ) {
-            items(listImages, key = { it }) { image ->
-                val itemIndex = listImages.indexOf(image)
-                val itemOffset = remember(
-                    listState.firstVisibleItemIndex,
-                    listState.firstVisibleItemScrollOffset
-                ) {
-                    calculateItemOffset(listState, itemIndex)
-                }
+            itemsIndexed(listImages) { index, it ->
+                val density = LocalDensity.current
 
-                // Calculate rotation based on the item's offset from the center
-                val rotation = if (itemOffset != null) {
-                    (centerOfViewport() - itemOffset).coerceIn(-30f, 30f) / 6
-                } else 0f
-
+                val screenWidthPx: Float = with(LocalDensity.current) { screenWidthDp.toPx() }
                 Box(
                     modifier = Modifier
-                        .size(100.dp)
-                        .graphicsLayer {
-                            rotationZ = rotation
-                        }
-                        .then(Modifier.clip(shape = RoundedCornerShape(12.dp)))
+                        .size(50.dp)
+
                 ) {
                     Image(
-                        painter = painterResource(Res.drawable.image_1),
+                        painter = painterResource(it),
                         contentDescription = "",
                         contentScale = ContentScale.Crop,
+                        modifier = Modifier.clip(shape = RoundedCornerShape(12.dp))
                     )
                 }
                 Spacer(Modifier.width(10.dp))
@@ -95,13 +88,4 @@ fun App() {
         }
     }
 
-
-}
-
-
-private fun calculateItemOffset(listState: LazyListState, itemIndex: Int): Float? {
-    val layoutInfo = listState.layoutInfo
-    val visibleItems = layoutInfo.visibleItemsInfo
-    val targetItem = visibleItems.find { it.index == itemIndex }
-    return targetItem?.offset?.toFloat()?.plus(targetItem?.size?.toFloat() / 2)
 }
